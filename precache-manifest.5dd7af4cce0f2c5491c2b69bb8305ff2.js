41776559b59c365599c41e6e49cf934211b105a8
Filianov Pavlo <filianov@Pavlo-MacBook-Pro.local>
2020-05-03 15:22:11 +0300

    Updates

---
+++ precache-manifest.5dd7af4cce0f2c5491c2b69bb8305ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66c8dc7b1395346e05f01de59cf27d7",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "4f72999332f535117ce3",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.0ce6ffb6.chunk.css"
+  },
+  {
+    "revision": "e7fece533406a0a701dd",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.22112891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.22112891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f72999332f535117ce3",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f365191c.chunk.js"
+  },
+  {
+    "revision": "74c71d939713e9bd1713",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.545b6b07.js"
+  }
+]);
